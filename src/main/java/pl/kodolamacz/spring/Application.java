9edd3732a2bdbac04fa97efcdd57967ce558e169
dynekpl <dynek82@gmail.com>
2017-10-29 10:05:08 +0100

    Annotations example

--- src/main/java/pl/kodolamacz/spring/Application.java
+++ src/main/java/pl/kodolamacz/spring/Application.java
@@ -1,10 +1,25 @@
 package pl.kodolamacz.spring;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
   public static void main(String[] args) {
-    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Beans.class);
+
+    Human human = context.getBean("createHuman", Human.class);
+    Leg leg = context.getBean(Leg.class);
+
+    System.out.println(human);
+    System.out.println(leg);
+
+    System.out.println(leg == human.getLeg());
+
+    Human unHuman = context.getBean("getUnnormalHuman", Human.class);
+    System.out.println(unHuman);
+    System.out.println(unHuman.getLeg());
+
+    context.close();
   }
+
 }
